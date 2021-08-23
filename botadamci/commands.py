be5c3c96908af2e939b57a6fcd50174e28d471e1
samedamci <samedamci@disroot.org>
2021-08-23 21:03:18 +0200

    Check if user is admin before action

--- botadamci/commands.py
+++ botadamci/commands.py
@@ -10,10 +10,13 @@ def mute(update: Update, context: CallbackContext):
     mod = Moderation(update, context)
     user = update.message["reply_to_message"].from_user
     try:
-        mod.mute(user.id)
-        update.message.reply_text(
-            f"ℹ User {UserURL(user)} has been muted.", parse_mode="markdown"
-        )
+        if mod.is_user_admin(update.message.from_user.id):
+            mod.mute(user.id)
+            update.message.reply_text(
+                f"ℹ User {UserURL(user)} has been muted.", parse_mode="markdown"
+            )
+        else:
+            update.message.reply_text("❌ Permission error.")
     except PermissionError:
         update.message.reply_text("❌ Permission error.")
 
@@ -23,10 +26,13 @@ def mute(update: Update, context: CallbackContext):
     mod = Moderation(update, context)
     user = update.message["reply_to_message"].from_user
     try:
-        mod.unmute(user.id)
-        update.message.reply_text(
-            f"ℹ User {UserURL(user)} has been unmuted.", parse_mode="markdown"
-        )
+        if mod.is_user_admin(update.message.from_user.id):
+            mod.unmute(user.id)
+            update.message.reply_text(
+                f"ℹ User {UserURL(user)} has been unmuted.", parse_mode="markdown"
+            )
+        else:
+            update.message.reply_text("❌ Permission error.")
     except PermissionError:
         update.message.reply_text("❌ Permission error.")
 
@@ -36,15 +42,18 @@ def ban(update: Update, context: CallbackContext):
     mod = Moderation(update, context)
     user = update.message["reply_to_message"].from_user
     try:
-        if mod.chat_id in FEDERATION:
-            for chat_id in FEDERATION:
-                mod.chat_id = chat_id
-                mod.ban(user.id)
-                context.bot.send_message(
-                    chat_id,
-                    f"ℹ User {UserURL(user)} has been banned in federation.",
-                    parse_mode="markdown",
-                )
+        if mod.is_user_admin(update.message.from_user.id):
+            if mod.chat_id in FEDERATION:
+                for chat_id in FEDERATION:
+                    mod.chat_id = chat_id
+                    mod.ban(user.id)
+                    context.bot.send_message(
+                        chat_id,
+                        f"ℹ User {UserURL(user)} has been banned in federation.",
+                        parse_mode="markdown",
+                    )
+        else:
+            update.message.reply_text("❌ Permission error.")
     except PermissionError:
         update.message.reply_text("❌ Permission error.")
 
@@ -54,14 +63,17 @@ def unban(update: Update, context: CallbackContext):
     mod = Moderation(update, context)
     user = update.message["reply_to_message"].from_user
     try:
-        if mod.chat_id in FEDERATION:
-            for chat_id in FEDERATION:
-                mod.chat_id = chat_id
-                mod.unban(user.id)
-                context.bot.send_message(
-                    chat_id,
-                    f"ℹ User {UserURL(user)} has been unbanned in federation.",
-                    parse_mode="markdown",
-                )
+        if mod.is_user_admin(update.message.from_user.id):
+            if mod.chat_id in FEDERATION:
+                for chat_id in FEDERATION:
+                    mod.chat_id = chat_id
+                    mod.unban(user.id)
+                    context.bot.send_message(
+                        chat_id,
+                        f"ℹ User {UserURL(user)} has been unbanned in federation.",
+                        parse_mode="markdown",
+                    )
+        else:
+            update.message.reply_text("❌ Permission error.")
     except PermissionError:
         update.message.reply_text("❌ Permission error.")
